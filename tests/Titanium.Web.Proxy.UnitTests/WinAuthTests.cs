96119a95206259d557cf2ff709b2d5a2e8764b33
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-05-28 16:50:17 +0200

    ci: One test platform dependent

--- tests/Titanium.Web.Proxy.UnitTests/WinAuthTests.cs
+++ tests/Titanium.Web.Proxy.UnitTests/WinAuthTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
 using Titanium.Web.Proxy.Http;
 using Titanium.Web.Proxy.Network.WinAuth;
 
@@ -9,9 +10,16 @@ namespace Titanium.Web.Proxy.UnitTests
     {
         [TestMethod]
         public void Test_Acquire_Client_Token()
-        {
-            var token = WinAuthHandler.GetInitialAuthToken("mylocalserver.com", "NTLM", new InternalDataStore());
-            Assert.IsTrue(token.Length > 1);
+        {            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var token = WinAuthHandler.GetInitialAuthToken("mylocalserver.com", "NTLM", new InternalDataStore());
+                Assert.IsTrue(token.Length > 1);
+            }
+            else
+            {
+                Assert.Inconclusive("This test can only be run on Windows.");
+            }
         }
     }
 }
