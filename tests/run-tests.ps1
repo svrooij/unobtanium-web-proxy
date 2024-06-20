51e5dc669d15b2455b6793e2e19b6ec10b4a46b6
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-06-20 21:54:40 +0200

    ci: Running tests the correct way

---
+++ tests/run-tests.ps1
@@ -0,0 +1,22 @@
+IF (!$outputDir) {
+    $outputDir = Join-Path $(Get-Location) "coverage"
+    Write-Output "Output directory not specified. Using default: $outputDir"
+}
+
+Write-Output "Coverage output directory: $outputDir"
+
+$unitTestProject = Join-Path $(Get-Location) "tests" "Titanium.Web.Proxy.UnitTests" "Titanium.Web.Proxy.UnitTests.csproj"
+$integrationTestProject = Join-Path $(Get-Location) "tests" "Titanium.Web.Proxy.IntegrationTests" "Titanium.Web.Proxy.IntegrationTests.csproj"
+
+$collectCoverageParam = '/p:CollectCoverage=true;CoverletOutputFormat=json%2clcov%2ccobertura;MergeWith=' + "$outputDir.json;CoverletOutput=$outputDir"
+$skipObsoleteParam = '/p:ExcludeByAttribute=Obsolete%2cGeneratedCodeAttribute%2cCompilerGeneratedAttribute'
+
+$unitExit = 0
+
+dotnet test $unitTestProject --configuration Release -v minimal --no-build --logger GitHubActions $collectCoverageParam $skipObsoleteParam -- RunConfiguration.CollectSourceInformation=true
+$unitExit = $LastExitCode
+dotnet test $integrationTestProject -v minimal --no-build --logger GitHubActions $collectCoverageParam $skipObsoleteParam -- RunConfiguration.CollectSourceInformation=true
+
+if ($unitExit -ne 0 -or $LastExitCode -ne 0) {
+    exit 1
+}
