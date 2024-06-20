4d14fc74a54bf4a8ebe81b3abdd95ce5990f0f75
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-06-20 23:54:39 +0200

    Bunch of tests

---
+++ tests/Titanium.Web.Proxy.UnitTests/Helpers/HttpHelperTestsBoundry.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy.Helpers;
+
+namespace Titanium.Web.Proxy.UnitTests.Helpers;
+public partial class HttpHelperTests
+{
+    [TestMethod]
+    public void GetBoundaryFromContentType_WithBoundary_ReturnsBoundary ()
+    {
+        var contentType = "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW";
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.AreEqual("----WebKitFormBoundary7MA4YWxkTrZu0gW", boundary.ToString());
+    }
+
+    [TestMethod]
+    public void GetBoundaryFromContentType_WithQuotedBoundary_ReturnsBoundaryWithoutQuotes ()
+    {
+        var contentType = "multipart/form-data; boundary=\"----WebKitFormBoundary7MA4YWxkTrZu0gW\"";
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.AreEqual("----WebKitFormBoundary7MA4YWxkTrZu0gW", boundary.ToString());
+    }
+
+    [TestMethod(), Ignore("Generated test does not work")]
+    public void GetBoundaryFromContentType_WithoutBoundary_ReturnsNull ()
+    {
+        var contentType = "text/html; charset=UTF-8";
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.IsNull(boundary);
+    }
+
+    [TestMethod, Ignore("Generated test does not work")]
+    public void GetBoundaryFromContentType_WithEmptyContentType_ReturnsNull ()
+    {
+        var contentType = "";
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.IsNull(boundary);
+    }
+
+    [TestMethod, Ignore("Generated test does not work")]
+    public void GetBoundaryFromContentType_WithNullContentType_ReturnsNull ()
+    {
+        string contentType = null;
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.IsNull(boundary);
+    }
+
+    [TestMethod]
+    public void GetBoundaryFromContentType_WithMultipleParameters_ReturnsCorrectBoundary ()
+    {
+        var contentType = "multipart/form-data; charset=utf-8; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW";
+        var boundary = HttpHelper.GetBoundaryFromContentType(contentType);
+        Assert.AreEqual("----WebKitFormBoundary7MA4YWxkTrZu0gW", boundary.ToString());
+    }
+}
