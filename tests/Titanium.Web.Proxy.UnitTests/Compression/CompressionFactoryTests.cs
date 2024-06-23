d75cf2004b931f581aa5776bb68e5bbbc6000a32
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-06-23 13:38:04 +0200

    Fixing the tests

--- tests/Titanium.Web.Proxy.UnitTests/Compression/CompressionFactoryTests.cs
+++ tests/Titanium.Web.Proxy.UnitTests/Compression/CompressionFactoryTests.cs
@@ -11,11 +11,11 @@ public class CompressionFactoryTests
     [TestMethod]
     [DataRow(HttpCompression.Gzip, typeof(GZipStream))]
     [DataRow(HttpCompression.Deflate, typeof(DeflateStream))]
-    [DataRow(HttpCompression.Brotli, typeof(BrotliSharpLib.BrotliStream))]
+    [DataRow(HttpCompression.Brotli, typeof(BrotliStream))]
     public void Create_ShouldReturnCorrectStream ( HttpCompression type, System.Type expectedType )
     {
         using var memoryStream = new MemoryStream();
-        var result = CompressionFactory.Create(type, memoryStream);
+        using var result = CompressionFactory.Create(type, memoryStream);
 
         Assert.IsInstanceOfType(result, expectedType);
     }
