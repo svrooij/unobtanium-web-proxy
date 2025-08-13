19431cca6ae1b20f2c2227fb4a1142c8b8e5eb42
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2025-08-13 21:46:17 +0200

    Removing old samples and old tests

--- examples/Titanium.Web.Proxy.Examples.Basic/SampleClientState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace Unobtanium.Web.Proxy.Examples.Basic
-{
-    public class SampleClientState
-    {
-        public StringBuilder PipelineInfo { get; } = new StringBuilder();
-    }
-}
